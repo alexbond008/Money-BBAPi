2a044c86648cb2b81dc76ecf536341ff583a124d
bociepka <bociepka1@gmail.com>
2025-08-22 07:37:27 +0000

    added project structure

---
+++ money-api/src/test/java/com/bbapi/money_api/MoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbapi.money_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
