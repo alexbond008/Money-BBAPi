0126aac486d2a7ab48e286c9dc4653b871e79688
Aleks <aleksandergornik@gmail.com>
2025-08-22 08:13:50 +0000

    StockPrice entity added

--- money-api/src/main/java/com/bbapi/money_api/entity/StockPrice.java
+++ money-api/src/main/java/com/bbapi/money_api/entity/StockPrice.java
@@ -1,5 +1,41 @@
 package com.bbapi.money_api.entity;
 
-public class StockPrice {
-    
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+@Entity
+public class StockPrice {    
+    private Integer price;
+    private String ticker;
+    private Date timestamp;
+
+    public StockPrice() {
+    }
+
+    public StockPrice(Integer price, String ticker, Date timestamp) {
+        this.price = price;
+        this.ticker = ticker;
+        this.timestamp = timestamp;
+    }
+
+// getters and setters
+    public Integer getPrice() {
+        return price;
+    }
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+    public String getTicker() {
+        return ticker;
+    }
+    public void setTicker(String ticker) {
+        this.ticker = ticker;
+    }
+    public Date getTimestamp() {
+        return timestamp;
+    }
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
 }
