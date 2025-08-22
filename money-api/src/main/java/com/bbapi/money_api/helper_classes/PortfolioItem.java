08bd1049ce2c4716b52ed064ea4fbc20fc067fa7
bociepka <bociepka1@gmail.com>
2025-08-22 08:18:31 +0000

    added PortfolioItem

--- money-api/src/main/java/com/bbapi/money_api/helper_classes/PortfolioItem.java
+++ money-api/src/main/java/com/bbapi/money_api/helper_classes/PortfolioItem.java
@@ -5,4 +5,42 @@ public class PortfolioItem {
     private int quantity;
     private Integer avgPrice;
     private Integer currentPrice;
+
+    public PortfolioItem() {
+    }
+
+    public PortfolioItem(String ticker, int quantity, Integer avgPrice, Integer currentPrice) {
+        this.ticker = ticker;
+        this.quantity = quantity;
+        this.avgPrice = avgPrice;
+        this.currentPrice = currentPrice;
+    }
+    
+    public String getTicker() {
+        return ticker;
+    }
+    public void setTicker(String ticker) {
+        this.ticker = ticker;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public Integer getAvgPrice() {
+        return avgPrice;
+    }
+    public void setAvgPrice(Integer avgPrice) {
+        this.avgPrice = avgPrice;
+    }
+    public Integer getCurrentPrice() {
+        return currentPrice;
+    }
+    public void setCurrentPrice(Integer currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+
+
+    
 }
