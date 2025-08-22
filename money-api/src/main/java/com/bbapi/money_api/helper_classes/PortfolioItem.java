7df4d17a470343dd05ad82301c15e7b90b92d67f
bociepka <bociepka1@gmail.com>
2025-08-22 08:16:29 +0000

    added PortfolioItem

---
+++ money-api/src/main/java/com/bbapi/money_api/helper_classes/PortfolioItem.java
@@ -0,0 +1,8 @@
+package com.bbapi.money_api.helper_classes;
+
+public class PortfolioItem {
+    private String ticker;
+    private int quantity;
+    private Integer avgPrice;
+    private Integer currentPrice;
+}
