b78f567a989cd47fdc4e13850a604dce5bd60ef8
paulinaborys <paulinaborys98@gmail.com>
2025-08-22 10:59:46 +0000

    StockPrice Controller Service ServiceImpl-without update.

---
+++ money-api/src/main/java/com/bbapi/money_api/service/StockPriceServiceImpl.java
@@ -0,0 +1,45 @@
+package com.bbapi.money_api.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.bbapi.money_api.entity.StockPrice;
+import com.bbapi.money_api.entity.StockPriceId;
+import com.bbapi.money_api.repository.StockPriceRepository;
+
+@Service
+public class StockPriceServiceImpl implements StockPriceService {
+
+    private StockPriceRepository stockPriceRepository;
+
+    public StockPriceServiceImpl(StockPriceRepository stockPriceRepository) {
+        this.stockPriceRepository = stockPriceRepository;
+    }
+
+    @Override
+    public List<StockPrice> getAllStockPrices() {
+        return stockPriceRepository.findAll();
+    }
+
+    @Override
+    public StockPrice getStockPriceById(StockPriceId id) {
+        return stockPriceRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public StockPrice addStockPrice(StockPrice stockPrice) {
+        return stockPriceRepository.save(stockPrice);
+    }
+
+    // @Override
+    // public StockPrice updateStockPrice(StockPrice stockPrice) {
+    //     return stockPriceRepository.save(stockPrice);
+    // }
+
+    @Override
+    public void deleteStockPrice(StockPriceId id) {
+        stockPriceRepository.deleteById(id);
+    }
+
+}
