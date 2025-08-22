c1999d3692f91696fc1f3b1b5173308c5f1bf279
paulinaborys <paulinaborys98@gmail.com>
2025-08-22 08:20:51 +0000

    HistoryItem Entity Class

--- money-api/src/main/java/com/bbapi/money_api/entity/HistoryItem.java
+++ money-api/src/main/java/com/bbapi/money_api/entity/HistoryItem.java
@@ -1,5 +1,57 @@
 package com.bbapi.money_api.entity;
 
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+
+@Entity
 public class HistoryItem {
+    private String ticker;
+    private Integer quantity;
+    private Integer price;
+    private Date timestamp;
+
+    public HistoryItem() {
+    }
+
+    public HistoryItem(String ticker, Integer quantity, Integer price, Date timestamp) {
+        this.ticker = ticker;
+        this.quantity = quantity;
+        this.price = price;
+        this.timestamp = timestamp;
+    }
+
+    // getters and setters
+    public String getTicker() {
+        return ticker;
+    }
+
+    public void setTicker(String ticker) {
+        this.ticker = ticker;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
     
 }
