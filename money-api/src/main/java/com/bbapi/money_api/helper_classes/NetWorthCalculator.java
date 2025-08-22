b690de4087555be58040d8160acc0a9c343a9933
bociepka <bociepka1@gmail.com>
2025-08-22 12:51:32 +0000

    Added constructor to NetWorthCalculator

--- money-api/src/main/java/com/bbapi/money_api/helper_classes/NetWorthCalculator.java
+++ money-api/src/main/java/com/bbapi/money_api/helper_classes/NetWorthCalculator.java
@@ -2,6 +2,7 @@ package com.bbapi.money_api.helper_classes;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -16,6 +17,11 @@ public class NetWorthCalculator {
         // private HistoryItemService historyItemService;
 
         private Logger logger = LoggerFactory.getLogger(NetWorthCalculator.class);
+
+        public NetWorthCalculator(NetWorthService netWorthService, StockPriceService stockPriceService) {
+            this.netWorthService = netWorthService;
+            this.stockPriceService = stockPriceService;
+        }
 
         @EventListener(ApplicationReadyEvent.class)
         public void doSomethingAfterStartup() {
